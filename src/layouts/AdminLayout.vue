f5a774833dc2fe532531fa6b1ff6c94bf3a2e097
Arituch <anon.ksl42@gmail.com>
2024-07-14 03:13:31 +0700

    feat: Add admin layout and routes

---
+++ src/layouts/AdminLayout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import FooterComponent from '@/components/FooterComponent.vue'
+import HeaderComponent from '@/components/HeaderComponent.vue'
+</script>
+<template>
+  <div class="container mx-auto">
+    <!-- Navbar content -->
+    <HeaderComponent />
+    <!-- Main content -->
+    <slot />
+    <!-- Footer content -->
+    <FooterComponent />
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -6,9 +6,19 @@ import ProfileView from "@/views/user/ProfileView.vue";
 import SearchView from "@/views/user/SearchView.vue";
 import SuccessView from "@/views/user/SuccessView.vue";
 
+import AdminLoginView from "@/views/admin/LoginView.vue";
+import AdminDashboardView from "@/views/admin/DashboardView.vue";
+import AdminProductsListView from "@/views/admin/product/ListView.vue";
+import AdminProductUpdateView from "@/views/admin/product/UpdateView.vue";
+import AdminOrderListView from "@/views/admin/order/ListView.vue";
+import AdminOrderDetailView from "@/views/admin/order/DetailView.vue";
+import AdminUserListView from "@/views/admin/user/ListView.vue";
+import AdminUserUpdateView from "@/views/admin/user/UpdateView.vue";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // user site
     {
       path: "/",
       name: "home",
@@ -38,6 +48,53 @@ const router = createRouter({
       path: "/success",
       name: "success",
       component: SuccessView,
+    },
+
+    // admin site
+    {
+      path: "/admin/login",
+      name: "admin-login",
+      component: AdminLoginView,
+    },
+    {
+      path: "/admin/dashboard",
+      name: "admin-dashboard",
+      component: AdminDashboardView,
+    },
+    {
+      path: "/admin/products",
+      name: "admin-product-list",
+      component: AdminProductsListView,
+    },
+    {
+      path: "/admin/products/create",
+      name: "admin-product-create",
+      component: AdminProductUpdateView,
+    },
+    {
+      path: "/admin/products/update/:id",
+      name: "admin-product-update",
+      component: AdminProductUpdateView,
+    },
+    {
+      path: "/admin/orders",
+      name: "admin-order-list",
+      component: AdminOrderListView,
+    },
+    {
+      path: "/admin/orders/:id",
+      name: "admin-order-detail",
+      component: AdminOrderDetailView,
+    },
+    {
+      path: "/admin/users",
+      name: "admin-user-list",
+      component: AdminUserListView,
+    },
+    {
+      path: "/admin/users/update/:id",
+      name: "admin-user-update",
+      component: AdminUserUpdateView,
     },
   ],
 });
