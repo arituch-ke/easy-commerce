23576d6bd2710b733d97381301d739b23ac4cc58
Arituch <anon.ksl42@gmail.com>
2024-07-14 13:04:07 +0700

    refactor: Update router configuration with layout metadata

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
+import UserLayout from "@/layouts/UserLayout.vue";
+import AdminLayout from "@/layouts/AdminLayout.vue";
+import AuthenticatedLayout from "@/layouts/AuthenticatedLayout.vue";
+
 import HomeView from "@/views/user/HomeView.vue";
 import CartView from "@/views/user/CartView.vue";
 import CheckoutView from "@/views/user/CheckoutView.vue";
@@ -23,31 +27,37 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: { layout: UserLayout },
     },
     {
       path: "/profile",
       name: "profile",
       component: ProfileView,
+      meta: { layout: UserLayout },
     },
     {
       path: "/search",
       name: "search",
       component: SearchView,
+      meta: { layout: UserLayout },
     },
     {
       path: "/cart",
       name: "cart",
       component: CartView,
+      meta: { layout: UserLayout },
     },
     {
       path: "/checkout",
       name: "checkout",
       component: CheckoutView,
+      meta: { layout: UserLayout },
     },
     {
       path: "/success",
       name: "success",
       component: SuccessView,
+      meta: { layout: UserLayout },
     },
 
     // admin site
@@ -55,46 +65,55 @@ const router = createRouter({
       path: "/admin/login",
       name: "admin-login",
       component: AdminLoginView,
+      meta: { layout: AuthenticatedLayout },
     },
     {
       path: "/admin/dashboard",
       name: "admin-dashboard",
       component: AdminDashboardView,
+      meta: { layout: AdminLayout },
     },
     {
       path: "/admin/products",
       name: "admin-product-list",
       component: AdminProductsListView,
+      meta: { layout: AdminLayout },
     },
     {
       path: "/admin/products/create",
       name: "admin-product-create",
       component: AdminProductUpdateView,
+      meta: { layout: AdminLayout },
     },
     {
       path: "/admin/products/update/:id",
       name: "admin-product-update",
       component: AdminProductUpdateView,
+      meta: { layout: AdminLayout },
     },
     {
       path: "/admin/orders",
       name: "admin-order-list",
       component: AdminOrderListView,
+      meta: { layout: AdminLayout },
     },
     {
       path: "/admin/orders/:id",
       name: "admin-order-detail",
       component: AdminOrderDetailView,
+      meta: { layout: AdminLayout },
     },
     {
       path: "/admin/users",
       name: "admin-user-list",
       component: AdminUserListView,
+      meta: { layout: AdminLayout },
     },
     {
       path: "/admin/users/update/:id",
       name: "admin-user-update",
       component: AdminUserUpdateView,
+      meta: { layout: AdminLayout },
     },
   ],
 });
